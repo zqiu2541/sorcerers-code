430f1c4f30fd29e07d01fb5fbca7113f42712be7
codecademy <ccuser@codecademy.com>
2017-07-19 11:18:35 +0000

    Add the final chapter.

--- add_test.rb
+++ add_test.rb
@@ -3,9 +3,9 @@ require "git"
 
 g = Git.open(".")
 
-if g.ls_files.has_key?("scene-1.txt")
-  puts "Use the Git command to add files to the staging area."
+if g.ls_files.has_key?("scene-4.txt")
   exit 0
 else
   exit 1
 end
+
